120a1946d102002d34b296bd68cd5f5a5b8f0da9
Carlos Mario Carmona <cm.carmona18@outlook.com>
2017-03-21 20:15:28 -0700

    Colors
    Change the main color

--- html/overall/head.php
+++ html/overall/head.php
@@ -1,5 +1,6 @@
 <meta charset="UTF-8">
 <title>Complat</title>
+
 <!--Start Favicon-->
 <link rel="apple-touch-icon" sizes="180x180" href="/views/img/favicon/apple-touch-icon.png">
 <link rel="shortcut icon" type="image/x-icon" href="/views/img/favicon/favicon.ico"/>	
@@ -11,10 +12,14 @@
 <meta name="msapplication-TileImage" content="/views/img/favicon/mstile-144x144.png">
 <meta name="theme-color" content="#3a3a3a">
 <!--End Favicon-->
+
 <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 <link rel="stylesheet" href="views/css/estilo.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 <script src="views/js/generales.js"></script>
 <script src="views/js/menu.js"></script>
 <script src="views/js/main.js"></script>
+
 <!--<script src="https://cloud.github.com/downloads/kopipejst/jqSnow/jquery.snow.js"></script>-->
+
+<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
